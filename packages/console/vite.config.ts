7d411f0d424adfc8456925bd8ab7dea281535b93
Loris Witschard <loris.wit@gmail.com>
2021-04-07 00:44:33 +0200

    build(console): generate bundle stats

--- packages/console/vite.config.ts
+++ packages/console/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import visualizer from "rollup-plugin-visualizer"
 
-export default defineConfig({
-    plugins: [vue()],
+export default defineConfig(({ mode }) => ({
+    plugins: [vue(), visualizer({ open: mode == "stats" })],
     resolve: { alias: { "typeorm": "typeorm/typeorm-model-shim" } },
     build: { outDir: "build" },
-})
+}))
