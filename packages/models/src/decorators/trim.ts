377e54a85483a9f73d08ed8458ddf5fabdbef612
Loris Witschard <loris.wit@gmail.com>
2021-04-10 19:10:13 +0200

    build(models): bump class-transformer to 0.3.2 to support browser

--- packages/models/src/decorators/trim.ts
+++ packages/models/src/decorators/trim.ts
@@ -1,5 +1,5 @@
 import { Transform, TransformOptions } from "class-transformer"
 
 export function Trim(options?: TransformOptions): ReturnType<typeof Transform> {
-    return Transform(value => typeof value === "string" ? value.trim() : value, options)
+    return Transform(({ value }) => typeof value === "string" ? value.trim() : value, options)
 }
