a7a595ccf4ce703c5d06c2eb7d318be45a6462f2
Loris Witschard <loris.wit@gmail.com>
2021-09-18 19:31:23 +0200

    feat(console): add ranking page

---
+++ packages/console/src/pages/ranking.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { computed } from "vue"
+import { useStore } from "../store/store"
+import http from "../utils/http"
+import { Registration } from "@frilan/models"
+import UserLink from "../components/user-link.vue"
+
+const store = useStore()
+
+const { event } = store.state
+const registrations = await http.getMany(`/events/${ event }/registrations?load=user`, Registration)
+registrations.sort((a, b) => b.score - a.score)
+
+let filtered = $(computed(() => registrations.filter(({ score }) => score > 0)))
+
+function getRank(index: number): number {
+  // handle tied scores
+  if (index > 0 && filtered[index].score === filtered[index - 1].score)
+    return getRank(index - 1)
+  else
+    return index + 1
+}
+</script>
+
+<template lang="pug">
+h1 Ranking
+
+table
+  tr(v-for="(registration, index) in filtered")
+    td {{ getRank(index) }}
+    td
+      user-link(:user="registration?.user")
+    td {{ registration.score }} pts
+</template>
+
+<style scoped lang="sass">
+
+</style>
--- packages/console/src/router.ts
+++ packages/console/src/router.ts
@@ -3,6 +3,7 @@ import Planning from "./pages/planning.vue"
 import Join from "./pages/join.vue"
 import Login from "./pages/login.vue"
 import Tournament from "./pages/tournament.vue"
+import Ranking from "./pages/ranking.vue"
 import User from "./pages/user.vue"
 import { store } from "./store/store"
 
@@ -14,6 +15,7 @@ const router = createRouter({
         { path: "/join", name: "join", component: Join, meta: { visitor: true } },
         { path: "/tournament/:id", name: "tournament", component: Tournament },
         { path: "/user/:name", name: "user", component: User },
+        { path: "/ranking", name: "ranking", component: Ranking },
     ],
 })
 
